21e11d9c170744845693821649ffca70ceb3f261
munstafa432-cloud <munstafa432@gmail.com>
2026-03-03 19:48:23 +0300

    Refactor MenuLayer modification and add logging
    Removed MenuLayer modification and added log info.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,6 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
-
 using namespace geode::prelude;
 
-class $modify(MenuLayer) {
-    bool init() {
-        if (!MenuLayer::init()) return false;
-
-        auto alert = FLAlertLayer::create("Geode", "Mod Loaded!", "OK");
-        alert->show();
-
-        return true;
-    }
-};
+$execute {
+    log::info("Mod loaded successfully!");
+}
